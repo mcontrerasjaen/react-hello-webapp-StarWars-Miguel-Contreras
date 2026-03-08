99aa395d64133c671e2c1ce42ebcb3aadea0fe81
Miguel Contreras <mcontrerasjaen@gmail.com>
2026-03-08 15:55:41 +0000

    ajuste de logo en todas las pages

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,28 +3,48 @@ import ScrollToTop from "../components/ScrollToTop";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { StarWarsBackground } from "../components/StarWarsBackground";
+import logoStarWars from "../assets/img/LogoStarWars.jpg"; // Asegúrate de importar el logo aquí
 
 export const Layout = () => {
     return (
         <div>
-            {/* Contenedor principal con flex-column y altura mínima para que el footer baje */}
             <div className="d-flex flex-column min-vh-100" style={{ position: "relative" }}>
                 <ScrollToTop />
                 
-                {/* 1. Fondo: z-index muy bajo para que no tape nada */}
+                {/* 1. Fondo */}
                 <StarWarsBackground showCrawl={false} /> 
-                
-                {/* 2. Header: Siempre arriba */}
+            
+                             
+                {/* 2. Header */}
                 <header style={{ position: "relative", zIndex: 1000 }}>
                     <Navbar />
                 </header>
 
-                {/* 3. Main: flex-grow-1 obliga al footer a irse al fondo de la pantalla */}
-                <main className="flex-grow-1" style={{ position: "relative", zIndex: 1 }}>
-                    <Outlet />
-                </main>
+              <div style={{ 
+    position: "absolute", 
+    top: "100px", // <--- Aumenta esto (estaba en 20px) para que baje
+    left: "40px", 
+    zIndex: 2000,
+    pointerEvents: "none"
+}}>
+    <img 
+        src={logoStarWars} 
+        alt="Star Wars Logo" 
+        style={{ 
+            height: "140px", 
+            width: "auto",
+            filter: "drop-shadow(0 0 10px rgba(255, 193, 7, 0.4))",
+            pointerEvents: "auto"
+        }} 
+    />
+</div>
 
-                {/* 4. Footer: Aseguramos z-index para que sea visible sobre el fondo */}
+                {/* 3. Main */}
+                 <main className="flex-grow-1" style={{ position: "relative", zIndex: 1 }}>
+                <Outlet />
+            </main>
+
+                {/* 4. Footer */}
                 <footer style={{ position: "relative", zIndex: 1000 }}>
                     <Footer />
                 </footer>
